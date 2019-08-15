2725a7803cb7bdc097e406d71d4c02f708dbbc39
dioz95 <advendiodesandros1@gmail.com>
2019-08-15 16:24:35 +0700

    initial commit

---
+++ precache-manifest.2fec9f188f5f986add8a45f31180c6cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a31a319af97efbec20e276658e277e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ba7449f6da703cba11c4",
+    "url": "/static/css/main.e4ea00a3.chunk.css"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/static/js/2.bae3d14d.chunk.js"
+  },
+  {
+    "revision": "ba7449f6da703cba11c4",
+    "url": "/static/js/main.20651369.chunk.js"
+  },
+  {
+    "revision": "244a77a7bb4101583953",
+    "url": "/static/js/runtime~main.9e8d0c72.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
